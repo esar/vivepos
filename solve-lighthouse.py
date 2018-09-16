update firmware to calculate and send position update firmware to only calc/send in processing window after sync pulses to avoid missed pulses fix wrong rotation matrix from solver change solver to use leastsq for more reliable results add verification and dump options to solve-lighthouse.py

solve-lighthouse.py
@@ -6,7 +6,7 @@ import math
 import argparse
 import serial
 import numpy as np
-from sympy import *
+import scipy.optimize
 
 TICK_CLOCK_MHZ = 5.0
 SWEEP_CENTER_IN_MICROSEC = 4000
@@ -18,8 +18,8 @@ def ticks2angle(ticks):
 
 
 def angles2vector(xAngle, yAngle):
-	plane1 = np.float32([+cos(xAngle), 0, -sin(xAngle)])
-	plane2 = np.float32([0, +cos(yAngle), +sin(yAngle)])
+	plane1 = np.double([+cos(xAngle), 0, -sin(xAngle)])
+	plane2 = np.double([0, +cos(yAngle), +sin(yAngle)])
 	vector = np.cross(plane2, plane1)
 	vector /= np.linalg.norm(vector)
 	return vector
@@ -48,7 +48,7 @@ def calcRotationMatrix(fromVector, toVector):
 	# Compute rotation matrix - re-expressed to show structure
 	return (
 		rcos * np.eye(3) +
-		rsin * np.float32([
+		rsin * np.double([
 			[ 0, -w,  v],
 			[ w,  0, -u],
 			[-v,  u,  0]
@@ -58,8 +58,6 @@ def calcRotationMatrix(fromVector, toVector):
 
 
 def calcSensorDistances(sensorGrid, sensorTicks):
-	# From https://github.com/osresearch/lighthouse/blob/master/solve-lighthouse
-
 	sensorPos = np.array([
 		[+sensorGrid/2, -sensorGrid/2, 0],
 		[+sensorGrid/2, +sensorGrid/2, 0],
@@ -68,12 +66,12 @@ def calcSensorDistances(sensorGrid, sensorTicks):
 	])
 
 	# Calc distances between sensors
-	r01 = N(np.linalg.norm(sensorPos[0] - sensorPos[1]))
-	r02 = N(np.linalg.norm(sensorPos[0] - sensorPos[2]))
-	r03 = N(np.linalg.norm(sensorPos[0] - sensorPos[3]))
-	r12 = N(np.linalg.norm(sensorPos[1] - sensorPos[2]))
-	r13 = N(np.linalg.norm(sensorPos[1] - sensorPos[3]))
-	r23 = N(np.linalg.norm(sensorPos[2] - sensorPos[3]))
+	r01 = np.linalg.norm(sensorPos[0] - sensorPos[1])
+	r02 = np.linalg.norm(sensorPos[0] - sensorPos[2])
+	r03 = np.linalg.norm(sensorPos[0] - sensorPos[3])
+	r12 = np.linalg.norm(sensorPos[1] - sensorPos[2])
+	r13 = np.linalg.norm(sensorPos[1] - sensorPos[3])
+	r23 = np.linalg.norm(sensorPos[2] - sensorPos[3])
 
 	v0 = angles2vector(ticks2angle(sensorTicks[0][0]), ticks2angle(sensorTicks[0][1]))
 	v1 = angles2vector(ticks2angle(sensorTicks[1][0]), ticks2angle(sensorTicks[1][1]))
@@ -87,34 +85,46 @@ def calcSensorDistances(sensorGrid, sensorTicks):
 	v13 = np.dot(v1, v3)
 	v23 = np.dot(v2, v3)
 
-	k0, k1, k2, k3 = symbols('k0, k1, k2, k3')
+	funcs = [
+		# See https://trmm.net/Lighthouse for derivation of equations
+		lambda k0,k1,k2,k3: k0**2 + k1**2 - 2*k0*k1*v01 - r01**2,
+		lambda k0,k1,k2,k3: k0**2 + k2**2 - 2*k0*k2*v02 - r02**2,
+		lambda k0,k1,k2,k3: k0**2 + k3**2 - 2*k0*k3*v03 - r03**2,
+		lambda k0,k1,k2,k3: k2**2 + k1**2 - 2*k2*k1*v12 - r12**2,
+		lambda k0,k1,k2,k3: k3**2 + k1**2 - 2*k3*k1*v13 - r13**2,
+		lambda k0,k1,k2,k3: k2**2 + k3**2 - 2*k2*k3*v23 - r23**2,
 
-	solution = nsolve((
-		k0**2 + k1**2 - 2*k0*k1*v01 - r01**2,
-		k0**2 + k2**2 - 2*k0*k2*v02 - r02**2,
-		k0**2 + k3**2 - 2*k0*k3*v03 - r03**2,
-		k2**2 + k1**2 - 2*k2*k1*v12 - r12**2,
-		k3**2 + k1**2 - 2*k3*k1*v13 - r13**2,
-		k2**2 + k3**2 - 2*k2*k3*v23 - r23**2,
-	),
-                (k0, k1, k2, k3),
-		(1000, 1000, 1000, 1000),
-		verify = False
-	)
+		# Prefer solutions where the lengths differ by less than the grid size
+		lambda k0,k1,k2,k3: max(sensorGrid, abs(k0-k1))-sensorGrid + 
+		                    max(sensorGrid, abs(k0-k2))-sensorGrid + 
+		                    max(sensorGrid, abs(k0-k3))-sensorGrid + 
+		                    max(sensorGrid, abs(k1-k2))-sensorGrid + 
+		                    max(sensorGrid, abs(k1-k3))-sensorGrid + 
+		                    max(sensorGrid, abs(k2-k3))-sensorGrid
+	]
+	func = lambda x: [f(*x) for f in funcs]
+	solution = scipy.optimize.leastsq(func, [1000, 1000, 1000, 1000])
+	solution = solution[0]
 
-	p0 = np.float32(v0 * solution[0])
-	p1 = np.float32(v1 * solution[1])
-	p2 = np.float32(v2 * solution[2])
-	p3 = np.float32(v3 * solution[3])
+	#print "solution: ", solution
 
-	err01 = np.linalg.norm(p0 - p1) - r01
-	err02 = np.linalg.norm(p0 - p2) - r02
-	err03 = np.linalg.norm(p0 - p3) - r03
-	err12 = np.linalg.norm(p1 - p2) - r12
-	err13 = np.linalg.norm(p1 - p3) - r13
-	err23 = np.linalg.norm(p2 - p3) - r23
+	points = [
+		np.double(v0 * solution[0]),
+		np.double(v1 * solution[1]),
+		np.double(v2 * solution[2]),
+		np.double(v3 * solution[3])
+	]
 
-	return ([p0, p1, p2, p3], [err01, err02, err03, err12, err13, err23])
+	errors = {
+		'01': np.linalg.norm(points[0] - points[1]) - r01,
+		'02': np.linalg.norm(points[0] - points[2]) - r02,
+		'03': np.linalg.norm(points[0] - points[3]) - r03,
+		'12': np.linalg.norm(points[1] - points[2]) - r12,
+		'13': np.linalg.norm(points[1] - points[3]) - r13,
+		'23': np.linalg.norm(points[2] - points[3]) - r23
+	}
+
+	return (points, errors)
 
 
 def solve(sensorGrid, sensorTicks):
@@ -130,7 +140,7 @@ def solve(sensorGrid, sensorTicks):
 	]
 
 	# Average the normals and convert to unit vector
-	averageNormal = np.float32([0, 0, 0])
+	averageNormal = np.double([0, 0, 0])
 	for normal in planeNormals:
 		averageNormal += normal
 	averageNormal /= len(planeNormals)
@@ -138,7 +148,9 @@ def solve(sensorGrid, sensorTicks):
 
 	# Calculate rotation from plane normal as seen by lighthouse
 	# to plane normal from the sensor's point of view
-	rotationMatrixToFlat = calcRotationMatrix(averageNormal, np.float32([0, 0, 1]))
+	# TODO: Use orientation from OOTX data to get a real "up" vector
+	#       instead of assuming that the sensor is pointing up
+	rotationMatrixToFlat = calcRotationMatrix(averageNormal, np.double([0, 0, -1]))
 	
 	# Calculate center of plane as seen by lighthouse
 	center1 = sensorPoints[0] + ((sensorPoints[2] - sensorPoints[0]) / 2)
@@ -148,7 +160,6 @@ def solve(sensorGrid, sensorTicks):
 	# Translate lighthouse and sensor points so that they are centered on the origin
 	# and rotate so they are flat (Z is stright up from the sensor plane)
 	lighthousePoint = rotationMatrixToFlat.dot(-averageCenter)
-	lighthouseNormal = rotationMatrixToFlat.dot([0, 0, 1])
 	sensorPoints[0] = rotationMatrixToFlat.dot(sensorPoints[0] - averageCenter)
 	sensorPoints[1] = rotationMatrixToFlat.dot(sensorPoints[1] - averageCenter)
 	sensorPoints[2] = rotationMatrixToFlat.dot(sensorPoints[2] - averageCenter)
@@ -156,18 +167,109 @@ def solve(sensorGrid, sensorTicks):
 
 	# Calculate the rotation around the sensor normal vector
 	# to get X and Y in the correct orientation and rotate the points into place
-	rotationMatrix = calcRotationMatrix(sensorPoints[1] - sensorPoints[0], np.float32([0, 1, 0]))
+	rotationMatrix = calcRotationMatrix(sensorPoints[1] - sensorPoints[0], np.double([0, 1, 0]))
 	lighthousePoint = rotationMatrix.dot(lighthousePoint)
-	lighthouseNormal = rotationMatrix.dot(lighthouseNormal)
 	sensorPoints[0] = rotationMatrix.dot(sensorPoints[0])
 	sensorPoints[1] = rotationMatrix.dot(sensorPoints[1])
 	sensorPoints[2] = rotationMatrix.dot(sensorPoints[2])
 	sensorPoints[3] = rotationMatrix.dot(sensorPoints[3])
 
 	# Calculate the rotation matrix to convert from lighthouse to world	
-	lighthouseRotationMatrix = calcRotationMatrix(np.float32([0, 0, 1]), lighthouseNormal)
+	lighthouseRotationMatrix = rotationMatrix.dot(rotationMatrixToFlat)
 
-	return (lighthouseRotationMatrix, lighthousePoint, sensorPoints, errors)
+	return {
+		'rotation': lighthouseRotationMatrix, 
+		'origin': lighthousePoint, 
+		'sensorPoints': sensorPoints, 
+		'errors': errors
+	}
+
+
+def intersectLines(origin1, ray1, origin2, ray2):
+	w0 = origin1 - origin2
+	a = ray1[0]*ray1[0] + ray1[1]*ray1[1] + ray1[2]*ray1[2]
+	b = ray1[0]*ray2[0] + ray1[1]*ray2[1] + ray1[2]*ray2[2]
+	c = ray2[0]*ray2[0] + ray2[1]*ray2[1] + ray2[2]*ray2[2]
+	d = ray1[0]*w0[0] + ray1[1]*w0[1] + ray1[2]*w0[2]
+	e = ray2[0]*w0[0] + ray2[1]*w0[1] + ray2[2]*w0[2]
+
+	denom = a*c - b*b
+
+	if denom > -0.00001 and denom < 0.00001:
+		return (False, 0, 0)
+
+	t = (b * e - c * d) / denom
+	p1 = ray1 * t
+	p1 += origin1
+
+	t = (a * e - b * d) / denom
+	p2 = ray2 * t
+	p2 += origin2
+
+	result = (p1 + p2) / 2
+	distance = np.linalg.norm(p1 - p2)
+	
+	return (True, result, distance)
+
+
+def verify(sensorGrid, ticks1, ticks2, lighthouse1, lighthouse2):
+	sensorPos = np.array([
+		[+sensorGrid/2, -sensorGrid/2, 0],
+		[+sensorGrid/2, +sensorGrid/2, 0],
+		[-sensorGrid/2, +sensorGrid/2, 0],
+		[-sensorGrid/2, -sensorGrid/2, 0]
+	])
+
+	for i in range(len(ticks1)):
+		ray1 = angles2vector(ticks2angle(ticks1[i][0]), ticks2angle(ticks1[i][1]))
+		ray2 = angles2vector(ticks2angle(ticks2[i][0]), ticks2angle(ticks2[i][1]))
+
+		ray1 = lighthouse1['rotation'].dot(ray1)
+		ray2 = lighthouse2['rotation'].dot(ray2)
+
+		success, result, distance = intersectLines(
+			lighthouse1['origin'], ray1,
+			lighthouse2['origin'], ray2
+		)
+
+		if success:
+			print "sensor %u:" % i
+			print "\tintersect distance: ", distance
+			print "\tlocation: ", result
+			print "\terror:    ", result - sensorPos[i]
+		else:
+			print "sensor %u: error, rays are parallel" % i
+
+
+def dumpAngle(line):
+	data = line.split(',', 4)
+	if len(data) == 5:
+		print "%s:    %5s:%5s    %5s:%5s" % tuple(data)
+
+def dumpPosition(line):
+	data = line.split(',', 4)
+	if len(data) == 5:
+		print "%s:    X: %8s    Y: %8s    Z: %8s    err: %s" % tuple(data)
+
+def dumpOotx(line):
+	print line
+
+def dumpData(input, sensor, angles, ootx, position):
+	while True:
+		line = input.readline()
+		if not line: break
+		line = line.strip()
+
+		if sensor != None:
+			if int(line[2:-1].split(',')[0]) != sensor:
+				continue
+
+		if line.startswith('A:') and angles:
+			dumpAngle(line[2:-1])
+		elif line.startswith('P:') and position:
+			dumpPosition(line[2:-1])
+		elif line.startswith('D:') and ootx:
+			dumpOotx(line[2:-1])
 
 
 def collectSamples(input, count):
@@ -189,10 +291,12 @@ def collectSamples(input, count):
 	n = 0
 	while n < count:
 		line = input.readline()
-		if not line.startswith('AT:'):
-			continue
 
-		line = line[line.find('(') + 1 : line.rfind(')')]
+		if not line.startswith('A:'):
+			continue
+		line = line[2:-1]
+
+		#line = line[line.find('(') + 1 : line.rfind(')')]
 		cols = line.split(',', 6)
 
 		i = int(cols[0])
@@ -222,38 +326,37 @@ def collectSamples(input, count):
 
 def printSolutionText(solutions):
 	for index, solution in enumerate(solutions):
-		lr, lp, sp, err = solution
 		print "lighthouse %u:" % index
 		print "\trotation: "
-		for row in lr:
+		for row in solution['rotation']:
 			print "\t\t", row
 		print "\tposition: "
-		print "\t\t", lp
+		print "\t\t", solution['origin']
 		print "\terrors: "
-		print "\t\t0->1: %.04fmm" % err[0]
-		print "\t\t0->2: %.04fmm" % err[1]
-		print "\t\t0->3: %.04fmm" % err[2]
-		print "\t\t1->2: %.04fmm" % err[3]
-		print "\t\t1->3: %.04fmm" % err[4]
-		print "\t\t2->3: %.04fmm" % err[5]
+		for name, value in solution['errors'].iteritems():
+			print "\t\t%s: %.04fmm" % (name, value)
 		print
 
 
 def printSolutionC(solutions):
-	print "static lighthouse_t lighhouses[%u] = {" % len(solutions)
+	print "static lighthouse_t g_lighthouses[%u] =" % len(solutions)
+	print "{"
 	for i, solution in enumerate(solutions):
-		lr, lp, sp, err = solution
 		print "\t{"
 		print "\t\t{"
-		for j, row in enumerate(lr):
-			if j != len(lr) - 1:
+		for j, row in enumerate(solution['rotation']):
+			if j != len(solution['rotation']) - 1:
 				c = ','
 			else:
 				c = ''
 			print "\t\t\t%.05f, %.05f, %.05f%s" % (row[0], row[1], row[2], c)
 		print "\t\t},"
 		print "\t\t{"
-		print "\t\t\t%.05f, %.05f, %.05f" % (lp[0], lp[1], lp[2])
+		print "\t\t\t%.05f, %.05f, %.05f" % (
+			solution['origin'][0], 
+			solution['origin'][1], 
+			solution['origin'][2]
+		)
 		print "\t\t}"
 		if i == len(solutions) - 1:
 			print "\t}"
@@ -264,7 +367,10 @@ def printSolutionC(solutions):
 
 def printSolutionGnuplot(solutions):
 	for solution in solutions:
-		lr, lp, sp, err = solution
+		lr = solution['rotation']
+		lp = solution['origin']
+		sp = solution['sensorPoints']
+		err = solution['errors']
 
 		v = lambda v: ' '.join([str(x) for x in v])
 
@@ -304,6 +410,21 @@ parser.add_argument('--count', type=int,
 parser.add_argument('--grid', type=int,
 	help='sensor grid spacing in mm (default 60)',
 	default=60)
+parser.add_argument('--verify', action='store_true',
+	help='verify result by triangulating sensor positions',
+	default=False)
+parser.add_argument('--dump-angles', action='store_true',
+	help='write angle data to stdout, don\'t calc solution',
+	default=False)
+parser.add_argument('--dump-ootx', action='store_true',
+	help='write OOTX data to stdout, don\'t calc solution',
+	default=False)
+parser.add_argument('--dump-pos', action='store_true',
+	help='write position data to stdout, don\'t calc solution',
+	default=False)
+parser.add_argument('--dump-sensor', type=int,
+	help='restrict dump output to a single sensor',
+	default=None)
 args = parser.parse_args()
 
 input = None
@@ -313,8 +434,13 @@ elif os.path.isfile(args.input):
 	input = open(args.input)
 elif os.path.exists(args.input):
 	input = serial.Serial(args.input, args.bps)
+	input.readline() # discard first, probably partial, line
 else:
 	print "can't open %s" % args.input
+	sys.exit(1)
+
+if args.dump_angles or args.dump_ootx or args.dump_pos:
+	dumpData(input, args.dump_sensor, args.dump_angles, args.dump_ootx, args.dump_pos)
 	sys.exit(1)
 
 lighthouse1, lighthouse2 = collectSamples(input, args.count)
@@ -329,4 +455,7 @@ elif args.format == 'gnuplot':
 	printSolutionGnuplot((solution1, solution2))
 else:
 	print 'unknown output format'
+
+if args.verify:
+	verify(args.grid, lighthouse1, lighthouse2, solution1, solution2)
 