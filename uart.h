469ce7c4d912eea904b0fc4608aa3ae03fa35925
esar <hacks@esar.org.uk>
2018-12-31 14:30:50 +0000

    Added license info

--- uart.h
+++ uart.h
@@ -1,3 +1,25 @@
+/*
+
+   Copyright (C) 2018 Stephen Robinson
+  
+   This file is part of vivepos
+  
+   vivepos is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+  
+   vivepos is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this code (see the file names COPING).  
+   If not, see <http://www.gnu.org/licenses/>.
+  
+*/
+
 
 void uartPutChar(char c, FILE* stream);
 char uartGetChar(FILE* stream);
